83079c97b3d2502586eed82aac67c07697e1e7c4
Maksim Dimitrijevic <mdimitrijevic7622rn@raf.rs>
2026-02-03 19:39:26 +0100

    Dockerized frontend project.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, useParams } from "react-router-dom"
+import {Routes, Route, Navigate, useParams, useLocation} from "react-router-dom"
 import Login from "@/pages/LoginPage.tsx"
 import WarehousePage from "@/pages/WarehousePage.tsx";
 import WarehouseStockPage from "@/pages/WarehouseStockPage.tsx";
@@ -16,6 +16,13 @@ export default function AppRoutes() {
 
 function WarehouseStockPageWrapper() {
     const { id } = useParams<{ id: string }>()
+    const location = useLocation()
+
+    // Ako korisnik dođe sa WarehousePage, ime je u state-u.
+    // Ako ukuca direktno URL, stavljamo fallback "Warehouse"
+    const name = (location.state)?.warehouseName || "Warehouse"
+
     if (!id) return <p>Invalid warehouse</p>
-    return <WarehouseStockPage warehouseId={parseInt(id)} />
+
+    return <WarehouseStockPage warehouseId={parseInt(id)} warehouseName={name} />
 }
